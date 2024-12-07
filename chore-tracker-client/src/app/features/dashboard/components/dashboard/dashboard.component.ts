9da1ba9b328fb9e0ee189859196f3f1eaa351d51
AGelstein <gelstein.andrew@gmail.com>
2024-12-07 08:27:51 -0600

    setting up a basic dash

--- chore-tracker-client/src/app/features/dashboard/components/dashboard/dashboard.component.ts
+++ chore-tracker-client/src/app/features/dashboard/components/dashboard/dashboard.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { Chore } from '../../interfaces/chore';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [DatePipe],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent {
+  
+  chores: Chore[] = [
+    { id:'1', name: 'Vacuum the living room', dueDate: new Date('2024-12-06') },
+    { id:'2', name: 'Wash the dishes', dueDate: new Date('2024-12-07') },
+    { id:'3', name: 'Laundry', dueDate: new Date('2024-12-08') },
+    { id:'4', name: 'Take out the trash', dueDate: new Date('2024-12-09') },
+    { id:'5', name: 'Water the plants', dueDate: new Date('2024-12-10') }
+  ];
 
 }
---
+++ chore-tracker-client/src/app/features/dashboard/interfaces/chore.ts
@@ -0,0 +1,5 @@
+export interface Chore {
+    id: string;
+    name: string;
+    dueDate: Date;
+  }
